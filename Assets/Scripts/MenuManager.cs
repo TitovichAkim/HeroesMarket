df9f807080f8b0c310325f01433339567a1b86a2
TitovichAkim <vas_pisar@mail.ru>
2023-05-25 15:02:27 +0300

    25.05 15:02

--- Assets/Scripts/MenuManager.cs
+++ Assets/Scripts/MenuManager.cs
@@ -56,7 +56,6 @@ public class MenuManager : MonoBehaviour
     {
         shopManager.coins += _reward;
         ReturnPanel.SetActive(false);
-        Debug.Log($"Добавлено {_reward}");
     }
 
     public IEnumerator SaveTheMoment ()
@@ -65,7 +64,6 @@ public class MenuManager : MonoBehaviour
         {
             PlayerPrefs.SetString("lastSessionEndTime",  DateTime.Now.ToString());
             PlayerPrefs.Save();
-            Debug.Log("Сохранено");
             yield return new WaitForSeconds(5);
         }
     } 
